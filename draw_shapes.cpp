a5c80955113bb48cb134499b6e10559c434f6ac1
Mohit Yadav <f2014074@pilani.bits-pilani.ac.in>
2018-03-05 14:11:32 +0530

    core features

---
+++ draw_shapes.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+int main()
+{
+	int w=100;
+	char atom_window[] = "Drawing 1:Atom";
+	char rook_window[]="Drawing 2:Rook";
+	Mat atom_image = Mat::zeros(w,w,CV_8UC3);
+	Mat rook_image = Mat::zeros(w,w,CV_8UC3);
+
+}
